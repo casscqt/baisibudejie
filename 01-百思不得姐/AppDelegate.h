5c0579c02a0512a1900a27e75b77baa39d9879dd
Cass <282365956@qq.com>
2016-02-15 17:00:02 +0800

    Initial Commit

---
+++ 01-百思不得姐/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  01-百思不得姐
+//
+//  Created by Cass on 16/2/15.
+//  Copyright © 2016年 Cass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 01-百思不得姐/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  01-百思不得姐
+//
+//  Created by Cass on 16/2/15.
+//  Copyright © 2016年 Cass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
